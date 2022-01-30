b17ae974b73beb93ea7fc799900c6c572b6408f5
Rem Storms <89358730+TheRemDev@users.noreply.github.com>
2022-01-29 21:00:06 -0800

    Player
    Added simple click to move for character network

---
+++ CyberPatientDemo_Unity/Assets/0 - Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    PhotonView photonView;
+    Camera mainCam;
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+        agent = GetComponent<NavMeshAgent>();
+        mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (!photonView.IsMine)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Player clicked on: " + hit.collider.name);
+                MoveToPoint(hit.point);
+            }
+        }
+    }
+
+    private void MoveToPoint(Vector3 point)
+    {
+        Debug.Log("Moving to position: " + point);
+        agent.SetDestination(point);
+        agent.isStopped = false;
+    }
+
+}
